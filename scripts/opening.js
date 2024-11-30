62b7c178b5907d6cbbd2831e7b101b27c455c674
Neo <neogd35@gmail.com>
2024-11-30 16:14:03 +0100

    Opening round 1 and 2 matchup results

--- scripts/opening.js
+++ scripts/opening.js
@@ -22,22 +22,32 @@ const big = `<img title="BIG Clan" src="images/bigclan.png">`;
 const virtusPro = `<img title="Virtus.pro" src="images/vp.png">`;
 const passionUA = `<img title="Passion UA" src="images/passion.png">`;
 
-roundOneMatches[0].innerHTML += `${furia}<p class="result">TBD</p>${gamerLegion}`;
-roundOneMatches[1].innerHTML += `${virtusPro}<p class="result">TBD</p>${mibr}`;
-roundOneMatches[2].innerHTML += `${liquid}<p class="result">TBD</p>${cloud9}`;
-roundOneMatches[3].innerHTML += `${complexity}<p class="result">TBD</p>${flyQuest}`;
-roundOneMatches[4].innerHTML += `${big}<p class="result">TBD</p>${passionUA}`;
-roundOneMatches[5].innerHTML += `${fnatic}<p class="result">TBD</p>${wildcard}`;
-roundOneMatches[6].innerHTML += `${theMongolz}<p class="result">TBD</p>${rareAtom}`;
-roundOneMatches[7].innerHTML += `${pain}<p class="result">TBD</p>${imperial}`;
+roundOneMatches[0].innerHTML += `${furia}<p class="result">8-13</p>${gamerLegion}`;
+roundOneMatches[1].innerHTML += `${virtusPro}<p class="result">7-13</p>${mibr}`;
+roundOneMatches[2].innerHTML += `${liquid}<p class="result">13-10</p>${cloud9}`;
+roundOneMatches[3].innerHTML += `${complexity}<p class="result">6-13</p>${flyQuest}`;
+roundOneMatches[4].innerHTML += `${big}<p class="result">13-9</p>${passionUA}`;
+roundOneMatches[5].innerHTML += `${fnatic}<p class="result">11-13</p>${wildcard}`;
+roundOneMatches[6].innerHTML += `${theMongolz}<p class="result">13-2</p>${rareAtom}`;
+roundOneMatches[7].innerHTML += `${pain}<p class="result">13-5</p>${imperial}`;
 
-for (i=0; i<roundTwoMatches.length; i++) {
-    roundTwoMatches[i].innerHTML += `${tbd}<p class="result">TBD</p>${tbd}`;
-}
+roundTwoMatches[0].innerHTML += `${fnatic}<p class="result">5-13</p>${cloud9}`;
+roundTwoMatches[1].innerHTML += `${big}<p class="result">5-13</p>${flyQuest}`;
+roundTwoMatches[2].innerHTML += `${theMongolz}<p class="result">13-6</p>${mibr}`;
+roundTwoMatches[3].innerHTML += `${complexity}<p class="result">9-13</p>${passionUA}`;
+roundTwoMatches[4].innerHTML += `${virtusPro}<p class="result">13-4</p>${rareAtom}`;
+roundTwoMatches[5].innerHTML += `${liquid}<p class="result">13-10</p>${wildcard}`;
+roundTwoMatches[6].innerHTML += `${imperial}<p class="result">11-13</p>${furia}`;
+roundTwoMatches[7].innerHTML += `${pain}<p class="result">10-13</p>${gamerLegion}`;
 
-for (i=0; i<roundThreeMatches.length; i++) {
-    roundThreeMatches[i].innerHTML += `${tbd}<p class="result">TBD</p>${tbd}`;
-}
+roundThreeMatches[0].innerHTML += `${big}<p class="result">TBD</p>${virtusPro}`;
+roundThreeMatches[1].innerHTML += `${mibr}<p class="result">TBD</p>${passionUA}`;
+roundThreeMatches[2].innerHTML += `${furia}<p class="result">TBD</p>${wildcard}`;
+roundThreeMatches[3].innerHTML += `${pain}<p class="result">TBD</p>${cloud9}`;
+roundThreeMatches[4].innerHTML += `${rareAtom}<p class="result">TBD</p>${fnatic}`;
+roundThreeMatches[5].innerHTML += `${liquid}<p class="result">TBD</p>${flyQuest}`;
+roundThreeMatches[6].innerHTML += `${theMongolz}<p class="result">TBD</p>${gamerLegion}`;
+roundThreeMatches[7].innerHTML += `${complexity}<p class="result">TBD</p>${imperial}`;
 
 for (i=0; i<roundFourMatches.length; i++) {
     roundFourMatches[i].innerHTML += `${tbd}<p class="result">TBD</p>${tbd}`;
